eb67c9dd8629f2902322f969635718ca88961003
r0binsahin <robin.sahin88@gmail.com>
2025-02-10 17:00:51 +0100

    renders numberOfInvoices

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -13,8 +13,9 @@ export default async function Page() {
   const latestInvoices = await fetchLatestInvoices();
   const allInvoices = await fetchCardData();
   const totalPaidInvoices = allInvoices.totalPaidInvoices;
-
   const totalPendingInvoices = allInvoices.totalPendingInvoices;
+
+  const numberOfInvoices = allInvoices.numberOfInvoices;
 
   return (
     <main>
@@ -24,7 +25,7 @@ export default async function Page() {
       <div className='grid gap-6 sm:grid-cols-2 lg:grid-cols-4'>
         <Card title='Collected' value={totalPaidInvoices} type='collected' />
         <Card title='Pending' value={totalPendingInvoices} type='pending' />
-        {/* <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> */}
+        <Card title='Total Invoices' value={numberOfInvoices} type='invoices' />
         {/* <Card
           title="Total Customers"
           value={numberOfCustomers}
