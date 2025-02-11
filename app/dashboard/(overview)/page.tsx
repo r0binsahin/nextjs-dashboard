9bba4b79471b542afe1bed7b43ba926b756b0c4d
r0binsahin <robin.sahin88@gmail.com>
2025-02-11 15:32:01 +0100

    imports cardwrapper

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -3,6 +3,8 @@ import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 
+import CardWrapper from '@/app/ui/dashboard/cards';
+
 import { Suspense } from 'react';
 import {
   LatestInvoicesSkeleton,
