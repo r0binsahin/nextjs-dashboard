21c3b90fd8060eae1329fe11c328adc162a8d2bc
r0binsahin <robin.sahin88@gmail.com>
2025-02-17 15:23:31 +0100

    replace pathname

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,10 +1,12 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import { useSearchParams } from 'next/navigation';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
 
   function handleSearch(term: string) {
     const params = new URLSearchParams(searchParams);
@@ -14,7 +16,10 @@ export default function Search({ placeholder }: { placeholder: string }) {
     } else {
       params.delete('query');
     }
+
+    replace(`${pathname}?${params.toString()}`);
   }
+
   return (
     <div className='relative flex flex-1 flex-shrink-0'>
       <label htmlFor='search' className='sr-only'>
